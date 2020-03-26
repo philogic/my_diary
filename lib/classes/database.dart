c49642a06e60ecb403cbb1dab952c2ec910bbd8a
Jakub Oblizajek <jakub.oblizajek@gmail.com>
2020-03-26 02:13:59 +0100

    Add local database related classes. Add data manipulation related classes.

--- lib/classes/database.dart
+++ lib/classes/database.dart
@@ -14,7 +14,7 @@ class DatabaseFileRoutines {
     return File('$path/local_data.json');
   }
 
-  Future<String> readEntries() async {
+  Future<String> readContents() async {
 
     try {
       
@@ -22,19 +22,81 @@ class DatabaseFileRoutines {
 
       if (!file.existsSync()) {
         print('File does not exist: ${file.absolute}');
-        await writeEntries('{"Entries": []}');
+        await writeContents('{"Contents": []}');
       }
       String contents = await file.readAsString();
       return contents;
 
     } catch (e) {
-      print('Error readEntries $e');
+      print('Error readContents $e');
       return '';
     }
   }
 
-  Future<File> writeEntries (String json) async {
+  Future<File> writeContents (String json) async {
     final file = await _localFile;
     return file.writeAsString('$json');
   }
+}
+
+Database databaseFromJson(String str) {
+  final dataFromJson = json.decode(str);
+  return Database.fromJson(dataFromJson);
+}
+
+String databaseToJson(Database data) {
+  final dataToJson = data.toJson();
+  return json.encode(dataToJson);
+}
+
+
+
+class Database {
+  List<Content> content;
+
+  Database({
+    this.content
+  });
+
+  factory Database.fromJson(Map<String, dynamic> json) => Database(
+    content: List<Content>.from(json["contents"].map((x) => Content.fromJson(x))),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "contents": List<dynamic>.from(content.map((x) => x.toJson())),
+  };
+}
+
+class Content {
+  String id;
+  String date;
+  String mood;
+  String note;
+
+  Content({
+    this.id,
+    this.date,
+    this.mood,
+    this.note
+  });
+
+  factory Content.fromJson(Map<String, dynamic> json) => Content(
+    id: json["id"],
+    date: json["date"],
+    mood: json["mood"],
+    note: json["note"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "date": date,
+    "mood": mood,
+    "note": note
+  };
+}
+
+class ContentEdit {
+  String action;
+  Content content;
+  ContentEdit({this.action, this.content});
 }
